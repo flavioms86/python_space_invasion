fc97d97f2e0084589502cf88fdcca7ffa65bde36
Flavio Silva <flavioms86@hotmail.com>
2024-09-28 10:58:45 -0300

    Adicionado opção para rodar em fullscreen

--- alien_invasion.py
+++ alien_invasion.py
@@ -19,6 +19,11 @@ class AlienInvasion:
             (self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
 
+        # Fullscreen mode
+        # self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+        # self.settings.screen_width = self.screen.get_rect().width
+        # self.settings.screen_height = self.screen.get_rect().height
+
         self.ship = Ship(self)
 
     def run_game(self):
