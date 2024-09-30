7f9d19eadb816ced2182d2b48f802eac346351f4
Flavio Silva <flavioms86@hotmail.com>
2024-09-30 09:52:15 -0300

    Atirando bullets

--- alien_invasion.py
+++ alien_invasion.py
@@ -60,6 +60,8 @@ class AlienInvasion:
         elif event.key == pygame.K_q:
             # Exit the games by pressing Q key.
             sys.exit()
+        elif event.key == pygame.K_SPACE:
+            self._fire_bullet()
 
     def _check_keyup_events(self, event):
         """Respond to key releases."""
@@ -68,9 +70,16 @@ class AlienInvasion:
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = False
     
+    def _fire_bullet(self):
+        """Create a new bullet and add it to the bullets group."""
+        new_bullet = Bullet(self)
+        self.bullets.add(new_bullet)
+    
     def _update_screen(self):
         # Redraw the screen during each pass through the loop.
         self.screen.fill(self.settings.bg_color)
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
         self.ship.blitme()
 
         # Make the most recently drawn screen visible.
