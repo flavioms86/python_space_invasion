228da134fd07b792787ff5ea3ac56f5ad9ee97bb
Flavio Silva <flavioms86@hotmail.com>
2024-09-30 10:07:47 -0300

    Refatorar codigo atirar bullets

--- alien_invasion.py
+++ alien_invasion.py
@@ -34,13 +34,7 @@ class AlienInvasion:
         while True:
             self._check_events()
             self.ship.update()
-            self.bullets.update()
-
-            # Get rid of bullets that have disappeared.
-            for bullet in self.bullets.copy():
-                if bullet.rect.bottom <= 0:
-                    self.bullets.remove(bullet)
-
+            self._update_bullets()
             self._update_screen()
             self.ship.update()
             self.clock.tick(60)
@@ -91,6 +85,17 @@ class AlienInvasion:
 
         # Make the most recently drawn screen visible.
         pygame.display.flip()
+    
+    def _update_bullets(self):
+        """Update position of bullets and get rid of old bullets."""
+        # Update bullets positions.
+        self.bullets.update()
+
+        # Get rid of bullets that have disappeared.
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
+
 
 if __name__ == '__main__':
     # Make a game instance, and run the game.
