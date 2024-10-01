62a60907b0e847478b7e5258f7b0653a43c8e030
Flavio Silva <flavioms86@hotmail.com>
2024-10-01 14:06:19 -0300

    Adicionar chamada linha aliens

--- alien_invasion.py
+++ alien_invasion.py
@@ -80,8 +80,21 @@ class AlienInvasion:
     
     def _create_fleet(self):
         """Create the fleet of aliens."""
-        # Make and Alien.
+        # Create an alien and keep adding aliens until there's no room left.
+        # Spacing between aliens is one alien width.
         alien = Alien(self)
+        alien_width = alien.rect.width
+
+        current_x = alien_width
+
+        while current_x < (self.settings.screen_width - 2 * alien_width):
+            new_alien = Alien(self)
+            new_alien_x = current_x
+            new_alien.rect.x = current_x
+            self.aliens.add(new_alien)
+            current_x += 2 * alien_width
+
+        
         self.aliens.add(alien)
     
     def _update_screen(self):
